198cbf900d3e7d64499b6b9f7752b0c7c071b14a
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-27 12:09:06 -0500

    Ahmed Belhadj - Node Express Sprint Challenge
    CRUD projects and getProjectActions

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const actionModel = require("./data/helpers/actionModel");
 const projectModel = require("./data/helpers/projectModel");
+const actionModel = require("./data/helpers/actionModel");
 
 const server = express();
 
@@ -12,5 +12,78 @@ server.use(helmet());
 server.use(cors({}));
 
 //routing/endpoints
+
+//projectModel
+server.get("/api/projects", (req, res) => {
+  projectModel
+    .get()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      return error;
+    });
+});
+
+server.get("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  projectModel
+    .get(id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      return error;
+    });
+});
+
+server.get("/api/projects/:id/actions", (req, res) => {
+  const { id } = req.params;
+  projectModel
+    .getProjectActions(id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      return error;
+    });
+});
+
+server.post("/api/projects", (req, res) => {
+  const project = req.body;
+  projectModel
+    .insert(project)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      return error;
+    });
+});
+
+server.put("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  projectModel
+    .update(id, changes)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      return error;
+    });
+});
+
+server.delete("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  projectModel
+    .remove(id)
+    .then(response => {
+      res.json({ response });
+    })
+    .catch(error => {
+      return error;
+    });
+});
 
 server.listen(8000, () => console.log("\n=== API running... ===\n"));
